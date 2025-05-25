96670f4c7bf4d96d3e7d614fb5c53a4175394a80
Loubnada <104575618+Loubnada@users.noreply.github.com>
2025-05-25 23:12:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
-app= Flask(__name__)
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
 @app.route('/')
-def home():
-  return "tp4 : bonjour depuis render ! votre app flask est bien en ligne "
-if __name__=='__main__':
-  app.run()
+def index():
+    return render_template('index.html')
+
+@app.route('/reserver', methods=['POST'])
+def reserver():
+    nom = request.form['nom']
+    service = request.form['service']
+    date = request.form['date']
+    heure = request.form['heure']
+    
+    # Ici tu peux enregistrer la réservation dans une base de données ou un fichier
+    print(f"Réservation reçue : {nom}, {service}, {date}, {heure}")
+    
+    return render_template('confirmation.html', nom=nom, service=service, date=date, heure=heure)
+
+if __name__ == '__main__':
+    app.run(debug=True)
